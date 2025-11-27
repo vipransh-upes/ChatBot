7d85699f60834c1831737a132376191d6afadcda
vipransh-upes <vipransh0228@gmail.com>
2025-11-27 11:49:06 +0000

    update some function of chatbot

--- src/chatbot.c
+++ src/chatbot.c
@@ -8,6 +8,7 @@
 void chatbot_loop(char *naam) {
     char msg[200];
     int i;
+    srand(time(NULL));  // ✅ Add this here
 
     printf("Bot: Nice to meet you %s\n", naam);
     printf("Bot: Type help for commands\n\n");
@@ -19,21 +20,57 @@ void chatbot_loop(char *naam) {
         msg[i] = '\0';
         clrscr();
 
-        if(strcmp(msg, "hi") == 0 || strcmp(msg, "hello") == 0) {
-            int r = rand() % 3;
-            if(r == 0) printf("Bot: Hello bro :)\n");
-            else if(r == 1) printf("Bot: Hi there!\n");
+        if(strcmp(msg, "hi")==0 || strcmp(msg, "hello")==0) {
+            int r = rand()%3;
+            if(r==0) printf("Bot: Hello bro :)\n");
+            else if(r==1) printf("Bot: Hi there!\n");
             else printf("Bot: Hey hey!\n");
         }
-        // ... (add all other command handlers here, including srand(time(NULL)) at start)
-        else if(strcmp(msg, "bye") == 0) {
+        else if(strcmp(msg,"how are you")==0) {
+            int r = rand()%3;
+            if(r==0) printf("Bot: I'm fine hehe\n");
+            else if(r==1) printf("Bot: Doing great!\n");
+            else printf("Bot: Surviving in C language XD\n");
+        }
+        else if(strcmp(msg,"time")==0) {
+            time_t t;
+            time(&t);
+            printf("Bot: Current time is %s", ctime(&t));
+        }
+        else if(strcmp(msg,"tell me a fact")==0) {
+            int r = rand()%3;
+            if(r==0) printf("Bot: Honey never spoils\n");
+            else if(r==1) printf("Bot: Bananas are berries\n");
+            else printf("Bot: Octopus has 3 hearts\n");
+        }
+        else if(strcmp(msg,"motivate me")==0) {
+            int r = rand()%3;
+            if(r==0) printf("Bot: You can do it!\n");
+            else if(r==1) printf("Bot: Never give up\n");
+            else printf("Bot: Hard work pays off bro\n");
+        }
+        else if(strcmp(msg,"love you bot")==0) {
+            printf("Bot: Love you too %s but only as friend hehe\n", naam);
+        }
+        else if(strcmp(msg,"joke")==0) {
+            int r = rand()%3;
+            if(r==0) printf("Bot: Too many bugs in my life XD\n");
+            else if(r==1) printf("Bot: Windows caught cold LOL\n");
+            else printf("Bot: Compiler ate my code\n");
+        }
+        else if(strcmp(msg,"help")==0) {
+            printf("\n--- Commands List ---\n");
+            printf("hi / hello\nhow are you\ntime\ntell me a fact\n");
+            printf("motivate me\nlove you bot\njoke\nbye\n\n");
+        }
+        else if(strcmp(msg,"bye")==0) {
             printf("Bot: Bye bye %s :) Have a nice day\n", naam);
             break;
         }
         else {
-            int r = rand() % 3;
-            if(r == 0) printf("Bot: I don't understand\n");
-            else if(r == 1) printf("Bot: Try help command\n");
+            int r = rand()%3;
+            if(r==0) printf("Bot: I don't understand\n");
+            else if(r==1) printf("Bot: Try help command\n");
             else printf("Bot: Something went wrong XD\n");
         }
     }
