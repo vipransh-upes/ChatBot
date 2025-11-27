61fada18ee5f4212a55a31e83d6317f6a33d96a0
vipransh-upes <vipransh0228@gmail.com>
2025-11-27 11:41:39 +0000

    update

--- src/utils.c
+++ src/utils.c
@@ -1,20 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "utils.h"
 
-void clrscr()
-{
-#ifdef _WIN32
-    system("cls");
-#else
-    system("clear");
-#endif
-}
-
-void remove_newline(char* str)
-{
-    size_t len = strlen(str);
-    if(len > 0 && str[len - 1] == '\n')
-        str[len - 1] = '\0';
+void clrscr(void) {
+    system("cls");  // Windows;
 }
