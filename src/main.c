438c474ed6294539f41d47b3608590c888dd81d5
vipransh-upes <vipransh0228@gmail.com>
2025-11-27 11:37:43 +0000

    update main due to error

--- src/main.c
+++ src/main.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 #include "chatbot.h"
 #include "utils.h"
 
-int main()
-{
-    char naam[50];
+void chatbot_loop(char *naam) {
+    char msg[200];
+    int i;
 
-    srand(time(NULL));
+    printf("Bot: Nice to meet you %s\n", naam);
+    printf("Bot: Type help for commands\n\n");
 
-    clrscr();
+    while(1) {
+        printf("You: ");
+        fgets(msg, 200, stdin);
+        for(i = 0; msg[i] != '\n'; i++);
+        msg[i] = '\0';
+        clrscr();
 
-    printf("Bot: Hello user :)\n");
-    printf("Bot: What is your name?\n");
-    printf("You: ");
-    fgets(naam, 50, stdin);
-    remove_newline(naam);
-
-    clrscr();
-
-    greet_user(naam);
-
-    chat_loop(naam);
-
-    return 0;
+        if(strcmp(msg, "hi") == 0 || strcmp(msg, "hello") == 0) {
+            int r = rand() % 3;
+            if(r == 0) printf("Bot: Hello bro :)\n");
+            else if(r == 1) printf("Bot: Hi there!\n");
+            else printf("Bot: Hey hey!\n");
+        }
+        // ... (add all other command handlers here, including srand(time(NULL)) at start)
+        else if(strcmp(msg, "bye") == 0) {
+            printf("Bot: Bye bye %s :) Have a nice day\n", naam);
+            break;
+        }
+        else {
+            int r = rand() % 3;
+            if(r == 0) printf("Bot: I don't understand\n");
+            else if(r == 1) printf("Bot: Try help command\n");
+            else printf("Bot: Something went wrong XD\n");
+        }
+    }
 }
