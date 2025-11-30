77f7761d0795f52bc64c830e0f3c6654c1fa6860
Vipransh Sharma <vipransh0228@gmail.com>
2025-11-30 22:57:03 +0530

    Update chatbot.c

--- src/chatbot.c
+++ src/chatbot.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include "chatbot.h"
+#include <stdio.h> 
+#include <string.h>  
+#include <stdlib.h>   
+#include <time.h>  
+#include "chatbot.h" 
 #include "utils.h"
 
 void chatbot_loop(char *naam) {
